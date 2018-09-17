7ae82988f30aa36daf9f96fa9b6b92fc80f16063
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-09-17 15:06:40 +0200

    libcmocka_shared

--- release_vars.mk
+++ release_vars.mk
@@ -4,12 +4,14 @@ LIBNAME = lib$(NAME)
 CALL_DIR := $(shell pwd)
 SRC_DIR := $(ROOT)/sources
 INC_DIR := $(ROOT)/includes
-OBJ_DIR := $(ROOT)/
+OBJ_DIR := $(ROOT)
 LIB_DIR := $(ROOT)/libs
-LIBS_L := $(LIB_DIR)/
-LIBS_I := $(LIB_DIR)/
+LIBS_L := $(LIB_DIR)
+LIBS_I := $(LIB_DIR)
 OUT_DIR_LIB := $(LIBS_L)
 OUT_DIR_H := $(LIBS_I)
+
+OBJS := $(patsubst %,$(OBJ_DIR)/%.o,$(TARGETS))
 
 CC := gcc
 CFLAGS_MORE =
